7a8e89d4b85bd40660417285a0bdf989b6948cba
Tim <tim8912097887@gmail.com>
2026-04-30 16:43:57 +0800

    feature(optional): add optional validator
    add optional validator that can use as last validator or wrap other
    validator.

--- src/my-zod/base.ts
+++ src/my-zod/base.ts
@@ -1,10 +1,10 @@
 import { MyZodError } from './error.js';
-import { ParseContext } from './type.js';
+import { infer as MyZodInfer, ParseContext } from './type.js';
 
 export abstract class MyZodType<T> {
     protected validators: Array<(ctx: ParseContext<T>) => boolean> = [];
     protected transformer: Array<(input: T) => T> = [];
-    protected abstract validate(ctx: ParseContext): ParseContext;
+    protected abstract validate(ctx: ParseContext<T>): ParseContext;
 
     parse(input: unknown): T {
         const result = this.safeParse(input);
@@ -17,7 +17,7 @@ export abstract class MyZodType<T> {
 
     safeParse(input: unknown) {
         // Type check
-        const result = this.validate({ value: input, issues: [] });
+        const result = this.validate({ value: input as T, issues: [] });
         if (result.issues.length > 0) {
             return { success: false, error: new MyZodError(result.issues) };
         }
@@ -37,9 +37,35 @@ export abstract class MyZodType<T> {
     transform<Out>(transformer: (input: T) => Out) {
         return new MyZodTransformer<Out, T>(this, transformer);
     }
+
+    optional() {
+        return optional(this);
+    }
 }
 
-export class MyZodTransformer<Out, In> extends MyZodType<Out> {
+export function optional<T extends MyZodType<any>>(schema: T) {
+    return new MyZodOptional(schema);
+}
+
+export class MyZodOptional<T extends MyZodType<any>> extends MyZodType<
+    undefined | MyZodInfer<T>
+> {
+    constructor(private schema: T) {
+        super();
+    }
+    protected validate(ctx: ParseContext) {
+        if (ctx.value === undefined) return ctx;
+        const result = this.schema.safeParse(ctx.value);
+        if (result.success) {
+            ctx.value = result.data;
+        } else if (result.error) {
+            ctx.issues.push(...result.error.issues);
+        }
+        return ctx;
+    }
+}
+
+class MyZodTransformer<Out, In> extends MyZodType<Out> {
     #inputSchema: MyZodType<In>;
     #transformer: (input: In) => Out;
 
@@ -50,7 +76,7 @@ export class MyZodTransformer<Out, In> extends MyZodType<Out> {
     }
 
     protected validate(ctx: ParseContext): ParseContext {
-        const result = this.#inputSchema.safeParse(ctx.value);
+        const result = this.#inputSchema.safeParse(ctx.value as In);
         if (result.data) {
             ctx.value = this.#transformer(result.data);
         } else if (result.error) {
--- src/my-zod/type.ts
+++ src/my-zod/type.ts
@@ -1,6 +1,10 @@
+import { MyZodType } from './base.js';
 import { MyZodIssue } from './error.js';
 
 export type ParseContext<T = unknown> = {
     value: T;
     issues: MyZodIssue[];
 };
+
+export type infer<T extends MyZodType<any>> =
+    T extends MyZodType<infer U> ? U : never;
